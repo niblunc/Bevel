81f6d4364e7e6500a7003f11ef14ac452ae3d8cf
NikkiBytes <nicholletteacosta@gmail.com>
2019-03-05 14:58:23 -0500

    added quality ana scripts

--- ana/feat2/.ipynb_checkpoints/make_design2.3_fsfs-checkpoint.py
+++ ana/feat2/.ipynb_checkpoints/make_design2.3_fsfs-checkpoint.py
@@ -16,7 +16,7 @@ for sub in SUB_DIRS:
     print("----------------------->>>>SUBJECT: ",sub)
     FEATS_PATH = os.path.join(sub, "func/Analysis/feat1/*.feat")
     FEATS = glob.glob(FEATS_PATH)
-    with open(os.path.join(DER_DIR, "design_files/design2.fsf"), 'r') as infile:
+    with open(os.path.join(DER_DIR, "design_files/design2.3.fsf"), 'r') as infile:
         tempfsf=infile.read()
         # set outpath for fsf OUTPATH variable, by run
         subID =  sub.split("/")[-1]
@@ -24,21 +24,10 @@ for sub in SUB_DIRS:
         print(">>>>>>>>>>>>>>>>>SETTING DESIGN OUTPATH: ", outpath)
         tempfsf = tempfsf.replace("OUTPUT", outpath)
         print(FEATS)
-        for run_path in FEATS:
-            run = run_path.split("/")[-1].split(".")[0].split("_")[1]
-            print(">>>>>------> RUN: ", run)
-            if run == "run1":
-                print("> %s : %s"%(run, run_path))
-                tempfsf = tempfsf.replace(run, run_path)
-            elif run == "run2":
-                print("> %s : %s"%(run, run_path))
-                tempfsf = tempfsf.replace(run, run_path)
-            elif run == "run3":
-                print("> %s : %s"%(run, run_path))
-                tempfsf = tempfsf.replace(run, run_path)
-            else:
-                print("> %s : %s"%(run, run_path))
-                tempfsf = tempfsf.replace(run, run_path)
+        for index,run_path in enumerate(FEATS):
+            run_id = "run%s"%(index+1)
+            print("> %s : %s"%(run_id, run_path))
+            tempfsf = tempfsf.replace(run_id, run_path)
         OUTFILE_PATH = os.path.join(FEAT2_DIR, "%s_design.fsf"%subject)
         print("OUTFILE ------------------------>>>> ", OUTFILE_PATH)
         with open(OUTFILE_PATH, "w") as outfile:
