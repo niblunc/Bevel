908fabb279646c35377e57c830d483f1fbf6a944
NikkiBytes <nicholletteacosta@gmail.com>
2019-09-18 23:54:46 -0400

    added beta updates

--- ana/beta/fsl/beta1_check.py
+++ ana/beta/fsl/beta1_check.py
@@ -5,6 +5,7 @@ beta_dict={}
 rerun_subs = []
 
 subs=glob.glob(os.path.join('/projects/niblab/bids_projects/Experiments/Bevel/derivatives', 'sub-*'))
+sub_ids = [x.split('/')[-1] for x in subs]
 for sub in sorted(subs):
     sub_id = sub.split("/")[-1]
     if sub_id not in beta_dict:
@@ -27,7 +28,17 @@ for sub in sorted(subs):
     if total_trial_ct == 0:
         print("\n*ID: {} \tTrial Count: {}".format(sub_id, total_trial_ct))
         rerun_subs.append(sub_id)
-        
+    run_ct = 0
+    if run1_trial_ct != 0:
+        run_ct += 1
+    if run2_trial_ct != 0:
+        run_ct += 1
+    if run3_trial_ct != 0:
+        run_ct += 1
+    if run4_trial_ct != 0:
+        run_ct += 1
+    beta_dict[sub_id]["TOTAL_RUNS_CT"] = run_ct
+    
 df=pd.DataFrame(beta_dict)
 df=df.T
 
@@ -40,19 +51,24 @@ notzero2=df["RUN2_TRIALS_CT"] != 0
 notzero3=df["RUN3_TRIALS_CT"] != 0
 notzero4=df["RUN4_TRIALS_CT"] != 0
 
-<<<<<<< HEAD
-zero_df = df[zero1 & zero2 & zero3 & zero4]
-=======
 all_zero_df = df[zero1 & zero2 & zero3 & zero4]
 zero_df = df[zero1 | zero2 | zero3 | zero4]
->>>>>>> e071f4bba980aa33cb29c6a05eca60b28c808194
 notzero_df1 =  df[notzero1 & notzero2 & notzero3 & notzero4]
 notzero_df2 =  df[notzero1 & notzero2 & notzero3 | notzero4]
 notzero_df3 =  df[notzero1 & notzero2 | notzero3 | notzero4]
 
 
 # list of subjects with trials done for all 4 runs - 
-four_runs_list = notzero_df1.index.tolist()
+all_runs_list = notzero_df1.index.tolist()
+missing_runs_list = [x for x in sub_ids if x not in all_runs_list]
 
+index = ["RUN1_TRIALS_CT", "RUN2_TRIALS_CT", "RUN3_TRIALS_CT", "RUN4_TRIALS_CT", "TOTAL_TRIALS_CT"]
+df_mia = pd.DataFrame(index=index, columns=missing_runs_list)
+df_T = df.T
+for x in sorted(missing_runs_list):
+    df_mia[x] = df_T[x]
+df_mia.sort_index(axis=1, inplace=True)
+
+df_mia.T.to_csv("inspect_runs.csv", sep="\t")
 df.to_csv("trial_count.csv", sep="\t")
 all_zero_df.to_csv("zero_trials.csv", sep="\t")
